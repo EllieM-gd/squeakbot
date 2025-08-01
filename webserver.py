622fef410d1c29ca57ad8dc680d8fa9fc39b86da
Ellie <151094173+EllieM-gd@users.noreply.github.com>
2025-07-31 18:44:50 -0700

    ServerFix

--- webserver.py
+++ webserver.py
@@ -1,13 +1,16 @@
 from flask import Flask
 from threading import Thread
+import os
 
 app = Flask('SqueakBot')
+
 @app.route('/')
 def home():
     return "SqueakBot is running!"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
 def keep_alive():
     t = Thread(target=run)
